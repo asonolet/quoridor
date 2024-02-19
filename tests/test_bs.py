ec4c34f11adf27ba7903c5fa31bc88ed19c72b3c
asonolet <aymeric.sonolet@gmail.com>
2024-02-19 22:16:19 +0100

    test add remove wall

--- tests/test_bs.py
+++ tests/test_bs.py
@@ -15,8 +15,15 @@ def test_next_player_changes() -> None:
     assert (first_player + board.next_player) % 2 == 1
     board.update_player_positions((4, 7, -1))
     assert (first_player + board.next_player) % 2 == 0
-    print(board.free_paths.keys())
     board.add_new_wall((3, 6, 0))
     assert (first_player + board.next_player) % 2 == 1
     board.remove_wall((3, 6, 0))
     assert (first_player + board.next_player) % 2 == 0
+
+
+def test_add_remove_wall() -> None:
+    board = bs.BoardState()
+    board.add_new_wall((4, 1, 0))
+    assert board.free_paths[41, 42] is False
+    board.remove_wall((4, 1, 0))
+    assert board.free_paths[41, 42] is True
